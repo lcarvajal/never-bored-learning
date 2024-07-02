63e9341228f527df3ec1dad62a41f501815dbbd9
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 00:13:24 +0200

    Configure firebase file for cloud messaging

---
+++ src/firebase.tsx
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+import { getMessaging, getToken } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "never-bored-learning.firebaseapp.com",
+  projectId: "never-bored-learning",
+  storageBucket: "never-bored-learning.appspot.com",
+  messagingSenderId: "456933280849",
+  appId: import.meta.env.FIREBASE_APP_ID
+};
+
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging();
+
+export const requestForToken = () => {
+  return getToken(messaging, { vapidKey: import.meta.env.VITE_FIREBASE_WEB_PUSH_CERTIFICATE_KEY })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log('current token for client: ', currentToken);
+        // Perform any other neccessary action with the token
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    })
+    .catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+    });
+};
