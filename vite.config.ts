24af7a9bd7a3c85543e22f9e8cbadb63f074b50e
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 00:12:57 +0200

    Configre environment variables with import.meta.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // FIXME: - Use only for dev environment
+    basicSsl()
+  ],
+  define: {
+    'process.env': {}
+  }
 })
