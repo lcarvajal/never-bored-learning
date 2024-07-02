22031efe93450354bfe17346640b9181973d3edf
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 00:12:35 +0200

    Configure environment variables for Firebase

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_FIREBASE_API_KEY: string
+  readonly VITE_FIREBASE_APP_ID: string
+  readonly VITE_FIREBASE_WEB_PUSH_CERTIFICATE_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
