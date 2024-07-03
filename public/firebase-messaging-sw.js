0c9871becfd716c4d64b88af63e0853ffa8f5502
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 14:23:08 +0200

    Add correct project id

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,21 +1,21 @@
-importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
   apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: "never-bored-learning.firebaseapp.com",
-  projectId: "never-bored-learning",
-  storageBucket: "never-bored-learning.appspot.com",
+  projectId: 'never-bored-learning',
+  storageBucket: 'never-bored-learning.appspot.com',
   messagingSenderId: "456933280849",
-  appId: process.env.VITE_FIREBASE_APP_ID
-};
+  appId: process.env.VITE_FIREBASE_APP_ID,
+});
 
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = getMessaging(firebaseApp);
 
 messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message ", payload);
