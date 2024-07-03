907496828876cdba4ce9d464977838848cb06a49
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 23:33:57 +0200

    Move componenets that behave as pages inside pages/

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+
+export default function LandingPage() {
+  return (
+    <div id="landing-page">
+      <h1>Never Bored Learning</h1>
+      <input></input>
+      <br></br>
+      <button>Start</button>
+    </div>
+  )
+}
