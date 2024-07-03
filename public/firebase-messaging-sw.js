3666b8bcd9ee49a3982a0311cbed629622f3b739
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 14:57:24 +0200

    Pass firebase app to message function

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,30 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseConfig = {
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+firebase.initializeApp({
   apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: "never-bored-learning.firebaseapp.com",
   projectId: 'never-bored-learning',
   storageBucket: 'never-bored-learning.appspot.com',
   messagingSenderId: "456933280849",
   appId: process.env.VITE_FIREBASE_APP_ID,
-};
+});
 
-const app = initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = app.messaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-
-  const notificationTitle = payload.notification.title;
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
   const notificationOptions = {
-    body: payload.notification.body,
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
