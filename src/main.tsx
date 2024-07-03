9ab286e7246a836fe9109c4828e6ca949fcdcef0
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-03 15:26:23 +0200

    Register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // Register the service worker
 if ('serviceWorker' in navigator) {
