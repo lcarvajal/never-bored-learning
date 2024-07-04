95d80800b3d88107c63d112ff6744879e20a8022
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-04 10:57:16 +0200

    Comment out remove button for demo

--- src/components/Tasks/Task/Task.tsx
+++ src/components/Tasks/Task/Task.tsx
@@ -14,18 +14,15 @@ interface TaskProps {
 export default function Task(props: TaskProps) {
   return (
     <div id={props.id.toString()} className="task">
-      <div className="task__close_button">
+      {/* <div className="task__close_button">
         <button onClick={() => props.onRemoveTask(props.id)}>X</button>
-      </div>
+      </div> */}
       <div className="task__title">
-        <p>
-          {props.title}
-          <br></br>
-          <span>{props.type}</span>
-        </p>
+        <h4>{props.title}</h4>
+        <p><span>{props.type}</span></p>
       </div>
       <div className="task__start_button">
-        <button onClick={() => props.onStartTask(props.id)}>Start</button>
+        <button onClick={() => props.onStartTask(props.id)}>↗</button>
       </div>
     </div>
   )
