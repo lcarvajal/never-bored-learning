acd9dfc47f9f7b01501961b3c9842189d7b9f711
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-13 18:06:33 +0200

    Add inputs and form for sign up page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -7,10 +7,36 @@ export default function SignUpPage() {
     navigate('/learning-overview');
   }
   return (
-    <div className="sign-up">
-      <h1>Sign Up</h1>
-      <br></br>
-      <button className="button-primary" onClick={handleSignUp}>Access Learning Path</button>
-    </div>
+    <form className="flex flex-col gap-2 w-full md:w-1/2 lg:w-1/3">
+      <h1 className="text-3xl mb-6">Create an account</h1>
+      <label htmlFor="email">Email</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="text"
+        id="password"
+        name="password"
+      />
+      <div className="my-3">
+        <input
+          type="checkbox"
+          id="subscribe"
+          name="subscribe"
+          className="w-4 h-4 mr-2"
+        />
+        <label htmlFor="subscribe" className="">Email me about new features</label>
+      </div>
+      <button
+        className="button-primary"
+        type="button"
+        onClick={handleSignUp}
+      >
+        Sign up
+      </button>
+    </form>
   )
 }
