71851659014cd7c194468fa07825c2e4b08e2591
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-13 16:30:32 +0200

    Change onboarding flow to landing page -> sign up -> learning path

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+
   function handleSignUp() {
     navigate('/learning-overview');
   }
