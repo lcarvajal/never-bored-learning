8b749e7ff2844901e7277a5c00f03fd42758d1c7
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-15 15:16:27 +0200

    Check if roadmap is set before loading

--- src/pages/LearningOverviewPage.tsx
+++ src/pages/LearningOverviewPage.tsx
@@ -38,27 +38,33 @@ export default function LearningOverviewPage() {
 
   return (
     <div className="flex flex-col grow gap-6">
-      <div className="border-slate-200 border-2 p-4 rounded-xl flex flex-col gap-4 mt-12">
-        <h1>Learning Roadmap</h1>
-        <p>{roadmap.learning_goal}</p>
-        {/* <div className="grid grid-cols-2 gap-4 w-60 ml-auto">
+      {roadmap && roadmap.modules ? (
+        <>
+          <div className="border-slate-200 border-2 p-4 rounded-xl flex flex-col gap-4 mt-12">
+            <h1>Learning Roadmap</h1>
+            <p>{roadmap.learning_goal}</p>
+            {/* <div className="grid grid-cols-2 gap-4 w-60 ml-auto">
           <button className="button-primary" onClick={handleOpenCurrentLearningGoal}>Open</button>
           <button className="button-primary">Complete</button>
         </div> */}
-      </div>
-      <div className="flex flex-col gap-4 w-10/12 mx-auto">
-        {roadmap.modules.map((item) => (
-          <div key={item.id} className="border-slate-200 border-2 p-4 rounded-xl flex flex-row gap-4">
-            <div className="flex flex-col grow">
-              <h2>{item.name}</h2>
-              <p>{item.description}</p>
-            </div>
-            <div className="w-20 text-center">
-              {/* <button className="button-primary">Start</button> */}
-            </div>
           </div>
-        ))}
-      </div>
+          <div className="flex flex-col gap-4 w-10/12 mx-auto">
+            {roadmap.modules.map((item) => (
+              <div key={item.id} className="border-slate-200 border-2 p-4 rounded-xl flex flex-row gap-4">
+                <div className="flex flex-col grow">
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                </div>
+                <div className="w-20 text-center">
+                  {/* <button className="button-primary">Start</button> */}
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
