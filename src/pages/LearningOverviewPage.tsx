31ad43281e129960eac83f7c2ba95057daf61ce0
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-15 15:01:44 +0200

    Implement get roadmaps end points from backend

--- src/pages/LearningOverviewPage.tsx
+++ src/pages/LearningOverviewPage.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 interface Roadmap {
-  goal: string,
-  items: RoadmapItem[],
+  learning_goal: string,
+  modules: RoadmapItem[],
 }
 
 interface RoadmapItem {
@@ -13,7 +13,7 @@ interface RoadmapItem {
 }
 
 export default function LearningOverviewPage() {
-  const [roadmap, setRoadmap] = useState<Roadmap>({ goal: "", items: [] } as Roadmap);
+  const [roadmap, setRoadmap] = useState<Roadmap>({} as Roadmap);
 
   useEffect(() => {
     axios.get('roadmaps')
@@ -40,14 +40,14 @@ export default function LearningOverviewPage() {
     <div className="flex flex-col grow gap-6">
       <div className="border-slate-200 border-2 p-4 rounded-xl flex flex-col gap-4 mt-12">
         <h1>Learning Roadmap</h1>
-        <p>{roadmap.goal}</p>
+        <p>{roadmap.learning_goal}</p>
         {/* <div className="grid grid-cols-2 gap-4 w-60 ml-auto">
           <button className="button-primary" onClick={handleOpenCurrentLearningGoal}>Open</button>
           <button className="button-primary">Complete</button>
         </div> */}
       </div>
       <div className="flex flex-col gap-4 w-10/12 mx-auto">
-        {roadmap.items.map((item) => (
+        {roadmap.modules.map((item) => (
           <div key={item.id} className="border-slate-200 border-2 p-4 rounded-xl flex flex-row gap-4">
             <div className="flex flex-col grow">
               <h2>{item.name}</h2>
