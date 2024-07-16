51c8ff636bd9fc7a302081f29ba2282c14528682
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-16 18:16:10 +0200

    Add indicator for loading categories

--- src/components/Tasks/TaskCategories.tsx
+++ src/components/Tasks/TaskCategories.tsx
@@ -15,25 +15,31 @@ export default function TaskCategories(props: TaskCategoriesProps) {
   return (
     <div>
       <div className="flex flex-row gap-2">
-        {categories.map((category, index) => (
-          <>
-            {props.selectedIndex === index ? (
-              <div className="border-2 border-slate-50 text-slate-50 px-2 rounded-xl"
-                key={index}
-              >
-                {category.name}
-              </div>
-            )
-              : (
-                <button className="bg-green-500 hover:bg-green-400 text-slate-900 px-2 rounded-xl"
+        {categories.length > 0 ?
+          (categories.map((category, index) => (
+            <>
+              {props.selectedIndex === index ? (
+                <div className="border-2 border-slate-50 text-slate-50 px-2 rounded-xl"
                   key={index}
-                  onClick={() => props.onSelectCategory(index)}
                 >
                   {category.name}
-                </button>
-              )}
-          </>
-        ))}
+                </div>
+              )
+                : (
+                  <button className="bg-green-500 hover:bg-green-400 text-slate-900 px-2 rounded-xl"
+                    key={index}
+                    onClick={() => props.onSelectCategory(index)}
+                  >
+                    {category.name}
+                  </button>
+                )}
+            </>
+          ))) : (
+            <>
+              <p>Loading categories...</p>
+            </>
+          )
+        }
       </div>
     </div>
   );
