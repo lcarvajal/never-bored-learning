5209ea724758d90cfccbc51e77c4232f511bbcc1
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-21 16:15:24 +0200

    Update js landing page copy

--- src/pages/public/JavaScriptLandingPage.tsx
+++ src/pages/public/JavaScriptLandingPage.tsx
@@ -12,7 +12,7 @@ export default function LandingPage() {
       {showSignUpForm ? <PasswordlessEmailLoginForm accountAction="SIGNIN" /> : (
         <div className="flex flex-col gap-6">
           <img src={jsImage} alt="javascript logo" className="w-32 mx-auto rounded-xl" />
-          <h1>Save time gathering resources to master JavaScript</h1>
+          <h1>Get resources to master JavaScript fast</h1>
           <button className="button-primary sm:mx-20" onClick={() => navigate('/create-learner-profile', { state: { goal: "I want to learn JavaScript because I " } })}>Start</button>
           <button className="text-violet-400" onClick={() => setShowSignUpForm(true)}>Already have an account?</button>
         </div>
