b0c3eaec54e410f7a7e4b6ad08f9f19ad645339d
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-27 01:13:04 +0200

    Implement new route for azure backend

--- src/pages/LearningOverviewPage.tsx
+++ src/pages/LearningOverviewPage.tsx
@@ -2,64 +2,33 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-interface UserProfile {
-  name: string;
-  email: string;
-  goal: string;
-  static_roadmaps: [string];
-}
-
 interface Roadmap {
+  id: number,
+  title: string,
+  owner_id: number,
   learning_goal: string,
-  modules: RoadmapItem[],
+  modules: Module[],
 }
 
-interface RoadmapItem {
+interface Module {
   id: number;
-  name: string;
+  title: string;
   description: string;
 }
 
 export default function LearningOverviewPage() {
   const navigate = useNavigate();
-  const [userProfile, setUserProfile] = useState<UserProfile>({} as UserProfile);
   const [roadmap, setRoadmap] = useState<Roadmap>({} as Roadmap);
-  const [isStaticRoadmap, setIsStaticRoadmap] = useState(false);
 
   useEffect(() => {
-    axios.get('profiles')
+    axios.get('/roadmaps')
       .then((response) => {
-        setUserProfile(response.data);
-
-        if (response.data.static_roadmaps && response.data.static_roadmaps.length > 0) {
-          setIsStaticRoadmap(true);
-          axios.get('/roadmaps/' + response.data.static_roadmaps[0])
-            .then((response) => {
-              setRoadmap(response.data);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }
-        else {
-          setIsStaticRoadmap(false);
-          axios.get('roadmaps')
-            .then((response) => {
-              setRoadmap(response.data);
-            })
-            .catch((error) => {
-              if (error.response.status === 404) {
-                axios.post('roadmaps')
-                  .then((response) => {
-                    setRoadmap(response.data);
-                  })
-                  .catch((error) => {
-                    console.log(error)
-                  })
-              } else {
-                console.log(error);
-              }
-            });
+        const roadmaps = response.data
+        if (roadmaps.length > 0) {
+          const roadmapId = roadmaps[0].id
+          axios.get(`/roadmaps/${roadmapId}/modules`).then((response) => {
+            setRoadmap(response.data);
+          })
         }
       })
       .catch((error) => {
@@ -69,34 +38,30 @@ export default function LearningOverviewPage() {
 
   return (
     <div className="flex flex-col grow gap-6">
-      {roadmap && roadmap.modules ? (
+      {(roadmap && roadmap.modules) && (
         <>
           <div className="rounded-xl flex flex-col gap-4 mt-12">
             <h1>Explore Resources</h1>
-            <p>{userProfile.goal}</p>
+            <p>{roadmap.learning_goal}</p>
           </div>
           <div className="flex flex-col gap-4">
-            {roadmap.modules.map((item) => (
-              <div key={item.id} className="group border-slate-200 border-2 p-4 rounded-xl flex flex-col sm:flex-row gap-4">
+            {roadmap.modules.map((module) => (
+              <div key={module.id} className="group border-slate-200 border-2 p-4 rounded-xl flex flex-col sm:flex-row gap-4">
                 <div className="flex flex-col grow w-full">
-                  <p className="font-bold">{item.name}</p>
-                  <p>{item.description}</p>
+                  <p className="font-bold">{module.title}</p>
+                  <p>{module.description}</p>
                 </div>
                 <div className="flex flex-col w-full sm:w-auto text-center justify-center">
                   {
-                    isStaticRoadmap && (
-                      <button className="button-primary sm:invisible sm:group-hover:visible ml-auto" onClick={() => { navigate('/roadmaps/' + userProfile.static_roadmaps[0] + "/" + item.id) }}>
-                        Open
-                      </button>
-                    )
+                    <button className="button-primary sm:invisible sm:group-hover:visible ml-auto" onClick={() => { navigate('/roadmaps/' + roadmap.id + "/" + module.id) }}>
+                      Open
+                    </button>
                   }
                 </div>
               </div>
             ))}
           </div>
         </>
-      ) : (
-        <p>Loading...</p>
       )}
     </div>
   )
