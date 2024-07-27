246d9523d03f0e1282947985dd394114deff9665
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-27 12:27:01 +0200

    Add clearer messages to user as resources are loaded

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -4,13 +4,6 @@ import { useParams } from 'react-router-dom';
 import Submodules from '../components/Tasks/Submodules';
 import axios from 'axios';
 
-interface Task {
-  title: string,
-  url: string,
-  description: string,
-  type: string
-}
-
 interface Module {
   "id": number,
   "title": string,
@@ -26,6 +19,13 @@ interface Submodule {
   "resources": Task[]
 }
 
+interface Task {
+  title: string,
+  url: string,
+  description: string,
+  type: string
+}
+
 export default function TasksPage() {
   const { roadmapId, moduleId } = useParams();
 
@@ -34,22 +34,28 @@ export default function TasksPage() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
-    axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`).then((response) => {
-      console.log("hello")
-      console.log("Response: ", response.data)
-      setModule(response.data);
-      if (response.data.submodules.length === 0) {
-        axios.post(`/roadmaps/${roadmapId}/modules/${moduleId}/populate`)
-        .then((response) => {
-          const subModules = response.data
-          setModule((module) => ({ ...module, submodules: subModules }));
-        }).catch((error) => {
-          console.log(error);
-        });
+    const fetchData = async () => {
+      try {
+        const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`)
+        setModule(module_response.data);
+  
+        if (module_response.data.submodules.length === 0) {
+          const submodule_response = await axios.post(`/roadmaps/${roadmapId}/modules/${moduleId}/populate`)
+          setModule((module) => ({ ...module, submodules: submodule_response.data }));
+
+          await new Promise(resolve => setTimeout(resolve, 3000));
+          const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}/submodules`)
+          setModule(module_response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    }).catch((error) => {
-      console.log(error);
-    })
+      finally {
+        handleSelectCategory(0)
+      }
+    }
+    
+    fetchData();
   }, []);
 
   function handleSelectCategory(index: number) {
@@ -58,19 +64,25 @@ export default function TasksPage() {
     setTasks(currentTasks);
   }
 
+  if (!module.title) {
+    return <p>Loading...</p>
+  }
+
+  if (module.submodules.length === 0) {
+    return <p>Breaking down {module.title} into submodules...</p>
+  }
+
+  if (module.submodules.filter((submodule) => submodule.resources.length > 0).length === 0) {
+    return <p>Loading resources...</p>
+  }
+
   return (
     <div className="flex flex-col grow gap-4 w-full md: w-3/3 lg:w-3/5 px-6">
+      <h1>{module.title}</h1>
+      <p>{module.description}</p>
+      <Submodules submodules={module.submodules} selectedIndex={selectedSubmoduleIndex} onSelectSubmodule={handleSelectCategory} />
       {
-        module.title && (
-          <>
-            <h1>{module.title}</h1>
-            <p>{module.description}</p>
-            <Submodules submodules={module.submodules} selectedIndex={selectedSubmoduleIndex} onSelectSubmodule={handleSelectCategory} />
-            {
-              tasks.length > 0 ? <Tasks tasks={tasks} /> : <p>Click a topic above to get started</p>
-            }
-          </>
-        )
+        tasks.length > 0 ? <Tasks tasks={tasks} /> : <p>The resources for this submodule are getting gathered. Try refreshing the page to load them.</p>
       }
     </div>
   )
