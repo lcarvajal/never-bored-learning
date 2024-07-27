af6f839c5c571c2519d5658063d2ded49933eb8c
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-27 22:23:15 +0200

    Fix a bug with loading

--- src/components/Tasks/Module.tsx
+++ src/components/Tasks/Module.tsx
@@ -31,6 +31,7 @@ interface ModuleProps {
 }
 
 export default function Module(props: ModuleProps) {
+  const [isLoading, setIsLoading] = useState(true);
   const [module, setModule] = useState<Module>({} as Module);
   const [selectedSubmoduleIndex, setSelectedSubmoduleIndex] = useState(-1);
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -43,6 +44,7 @@ export default function Module(props: ModuleProps) {
   }, []);
 
   const fetchData = async () => {
+    setIsLoading(true);
     try {
       const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`)
       setModule(module_response.data);
@@ -51,42 +53,30 @@ export default function Module(props: ModuleProps) {
         const submodule_response = await axios.post(`/roadmaps/${roadmapId}/modules/${moduleId}/populate`)
         setModule((module) => ({ ...module, submodules: submodule_response.data }));
 
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 6000));
         const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`)
+        console.log("RECEIVED DATA")
+        console.log(module_response.data)
         setModule(module_response.data);
+        setIsLoading(false);
+      }
+      else {
+        setIsLoading(false);
       }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-    finally {
-      if(module.submodules && module.submodules[0] && module.submodules[0].resources && module.submodules[0].resources.length > 0) {
-        handleSelectCategory(0)
-      }
-    }
   }
 
-  // const createResourcesForSubmodule = async (submoduleId: number, index: number) => {
-  //   try {
-  //     await axios.post(`/roadmaps/${roadmapId}/modules/${moduleId}/submodules/${submoduleId}/populate`)
-  //     const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`)
-  //     setModule(module_response.data);
-  //     handleSelectCategory(index);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   }
-  // }
-
   const fetchModule = async (index: number) => {
+    setIsLoading(true);
     try {
-      await new Promise(resolve => setTimeout(resolve, 3000));
       const module_response = await axios.get(`/roadmaps/${roadmapId}/modules/${moduleId}`)
-      if (module_response.data.submodules[index].resources.length === 0) {
-        fetchModule(index);
-      }
-      else {
-        setModule(module_response.data); 
-        handleSelectCategory(index);
-      }
+      setModule(module_response.data); 
+      setSelectedSubmoduleIndex(index);
+      setTasks(module_response.data.submodules[index].resources);
+      handleSelectCategory(index);
+      setIsLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -94,18 +84,15 @@ export default function Module(props: ModuleProps) {
 
   function handleSelectCategory(index: number) {
     setSelectedSubmoduleIndex(index);
-    const currentTasks = module.submodules[index].resources;
-    if (currentTasks) {
-      if (currentTasks.length === 0) {
-        setTasks([]);
+      const currentTasks = module.submodules[index].resources;
+      if (currentTasks) {
+        if (currentTasks.length === 0) {
+          setTasks([]);
+        }
+        else {
+          setTasks(currentTasks);
+        }
       }
-      else {
-        setTasks(currentTasks);
-      }
-    }
-    else {
-      fetchModule(index);
-    }
   }
 
   if (!module.title) {
@@ -116,6 +103,10 @@ export default function Module(props: ModuleProps) {
     return <p>Breaking down {module.title} into submodules...</p>
   }
 
+  if (isLoading === true) {
+    return <p>Scraping resources of the web...</p>
+  }
+
   return (
     <>
       <h1>{module.title}</h1>
@@ -124,10 +115,13 @@ export default function Module(props: ModuleProps) {
       {
       tasks.length > 0 ? <Tasks tasks={tasks} /> : (
         <>
-        { selectedSubmoduleIndex === -1 ? (
+        { (selectedSubmoduleIndex === -1) ? (
             <p>Select a submodule above to get started</p>
           ) : (
-            <p>Gathering resources for submodule...</p>
+            <>
+            <p>It can take a couple of minutes to gather resources.</p>
+            <button className='button-primary w-[20em]' onClick={() => fetchModule(selectedSubmoduleIndex)}>Try fetching resources again</button>
+            </>
           ) 
         }
         </>
