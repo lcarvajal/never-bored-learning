ceff99121e71b97fcb67338b62a546d7e9032360
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-27 20:30:18 +0200

    Convert string to number

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -7,7 +7,7 @@ export default function TasksPage() {
   return (
     <div className="flex flex-col grow gap-4 w-full md: w-3/3 lg:w-3/5 px-6">
       {
-        (roadmapId && moduleId) && <Module roadmapId={roadmapId} moduleId={moduleId} />
+        (roadmapId && moduleId) && <Module roadmapId={Number(roadmapId)} moduleId={Number(moduleId)} />
       }
     </div>
   )
