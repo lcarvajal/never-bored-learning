9862b70744f82c627d6c3cbbace745200c8f10c1
Lukas Carvajal <lukascarvajal@gmail.com>
2024-08-01 01:52:33 +0200

    Remove module descriptions

--- src/components/Roadmap/Modules.tsx
+++ src/components/Roadmap/Modules.tsx
@@ -17,19 +17,15 @@ export default function Modules(props: ModulesProps) {
     <>
     {modules.length > 0 && (
       <>
-        <div className="px-4 mb-4">
-          <h2 className="text-sm text-slate-400">Modules</h2>
-        </div>
         <div className="flex flex-col gap-4 align-items-start">
           {modules.map((module) => (
             <div 
               key={module.id} 
-              className="group p-4 rounded-xl flex flex-col gap-4 hover:bg-zinc-900 text-zinc-400 hover:text-slate-50 cursor-pointer"
+              className="group px-4 py-2 rounded-xl flex flex-col gap-4 hover:bg-zinc-900 text-zinc-400 hover:text-slate-50 cursor-pointer"
               onClick={ () => props.handleOpenModule(module.id) }
               >
               <div className="flex flex-col grow w-full">
-                <p className="font-medium text-lg">{module.title}</p>
-                <p className="text-sm group-hover:text-slate-200">{module.description}</p>
+                <p className="font text">{module.title}</p>
               </div>
             </div>
           ))}
